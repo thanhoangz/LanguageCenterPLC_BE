8cb7ea5dca2cfe0a2f2ac755c8d10d381a23cba5
phương nguyễn <nguyenvietphuong1010@outlook.com>
2019-10-13 20:22:00 +0700

    ok

---
+++ LanguageCenterPLC.Data.EF/Migrations/20191013072743_initialize_db.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LanguageCenterPLC.Data.EF.Migrations
+{
+    public partial class initialize_db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LanguageClasses_Courses_CourseId",
+                table: "LanguageClasses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "LanguageClasses",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LanguageClasses_Courses_CourseId",
+                table: "LanguageClasses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LanguageClasses_Courses_CourseId",
+                table: "LanguageClasses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "LanguageClasses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LanguageClasses_Courses_CourseId",
+                table: "LanguageClasses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
